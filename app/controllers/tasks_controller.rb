848a51bce326288f52d3096a8dca7510de4d4196
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-08-07 14:12:08 -0600

    Added due dates to index page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
 class TasksController < ApplicationController
 
   def new
-  @task = Task.new
+    @task = Task.new
     @task_list = TaskList.find(params[:id])
   end
 
@@ -12,23 +12,23 @@ class TasksController < ApplicationController
       due_date: parse_due_date(params[:start_date])
     )
 
-      if @task.save
-        flash[:notice] = "Task was created successfully!"
-        redirect_to root_path
-      else
-        render :new
-      end
-  end
-
-    def parse_due_date(due_date)
-      if due_date
-        string_date = due_date.to_s
-        string_date.to_date
-      end
+    if @task.save
+      flash[:notice] = "Task was created successfully!"
+      redirect_to root_path
+    else
+      render :new
     end
   end
 
+  def parse_due_date(due_date)
+    if due_date
+      string_date = due_date.to_s
+      string_date.to_date
+    end
 
+
+  end
+end
 
 
 
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,13 @@
 class Task < ActiveRecord::Base
   belongs_to :task_list
   validates_presence_of :name, {message: "Your task could not be created"}
+
+  def parse_due_date(due_date)
+    if due_date
+      string_date = due_date.to_s
+      string_date.to_date
+    end
   end
+
+end
+
