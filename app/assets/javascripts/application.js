28d491cd346738db6c1e7036d0626999d68477f0
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-08-11 15:02:17 -0600

    Search bar with background Image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,10 +3,20 @@
 //= require_tree .
 
 
-function MsgBox (Kelso) {
-  alert (Kelso) }
-
-function changecolor(code) {
-
-  document.bgColor=code
-}
+(function() {
+  var id = document.getElementById('search-form');
+  if (id && id.txtInput) {
+    var name = id.txtInput;
+    var unclicked = function() {
+      if (name.value == '') {
+        name.style.background = '#FFFFFF url(images/googbg.png) left no-repeat';
+      }
+    };
+    var clicked = function() {
+      name.style.background = '#ffffff';
+    };
+    name.onfocus = clicked;
+    name.onblur = unclicked;
+    unclicked();
+  }
+})();
