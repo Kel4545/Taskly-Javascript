c2f126e5819a320fbf4305b93e2c85a7c283e316
Kelly Pederson <Kelly.Pederson@gmail.com>
2014-08-11 15:07:18 -0600

    Lazy Image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,20 +3,76 @@
 //= require_tree .
 
 
-(function() {
-  var id = document.getElementById('search-form');
-  if (id && id.txtInput) {
-    var name = id.txtInput;
-    var unclicked = function() {
-      if (name.value == '') {
-        name.style.background = '#FFFFFF url(images/googbg.png) left no-repeat';
+!function(window){
+  var $q = function(q, res){
+      if (document.querySelectorAll) {
+        res = document.querySelectorAll(q);
+      } else {
+        var d=document
+          , a=d.styleSheets[0] || d.createStyleSheet();
+        a.addRule(q,'f:b');
+        for(var l=d.all,b=0,c=[],f=l.length;b<f;b++)
+          l[b].currentStyle.f && c.push(l[b]);
+
+        a.removeRule(0);
+        res = c;
       }
-    };
-    var clicked = function() {
-      name.style.background = '#ffffff';
-    };
-    name.onfocus = clicked;
-    name.onblur = unclicked;
-    unclicked();
+      return res;
+    }
+    , addEventListener = function(evt, fn){
+      window.addEventListener
+        ? this.addEventListener(evt, fn, false)
+        : (window.attachEvent)
+        ? this.attachEvent('on' + evt, fn)
+        : this['on' + evt] = fn;
+    }
+    , _has = function(obj, key) {
+      return Object.prototype.hasOwnProperty.call(obj, key);
+    }
+    ;
+
+  function loadImage (el, fn) {
+    var img = new Image()
+      , src = el.getAttribute('data-src');
+    img.onload = function() {
+      if (!! el.parent)
+        el.parent.replaceChild(img, el)
+      else
+        el.src = src;
+
+      fn? fn() : null;
+    }
+    img.src = src;
   }
-})();
+
+  function elementInViewport(el) {
+    var rect = el.getBoundingClientRect()
+
+    return (
+      rect.top    >= 0
+      && rect.left   >= 0
+      && rect.top <= (window.innerHeight || document.documentElement.clientHeight)
+      )
+  }
+
+  var images = new Array()
+    , query = $q('img.lazy')
+    , processScroll = function(){
+      for (var i = 0; i < images.length; i++) {
+        if (elementInViewport(images[i])) {
+          loadImage(images[i], function () {
+            images.splice(i, i);
+          });
+        }
+      };
+    }
+    ;
+  // Array.prototype.slice.call is not callable under our lovely IE8
+  for (var i = 0; i < query.length; i++) {
+    images.push(query[i]);
+  };
+
+  processScroll();
+  addEventListener('scroll',processScroll);
+
+}(this);
